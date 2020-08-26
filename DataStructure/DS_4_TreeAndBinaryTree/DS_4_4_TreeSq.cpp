76bb9eba41bd3b9b0022c576e656b38379574ad5
Kim Yang <kim_yang95@163.com>
2020-08-26 17:06:53 +0800

    :seedling: add Tree

---
+++ DataStructure/DS_4_TreeAndBinaryTree/DS_4_4_TreeSq.cpp
@@ -0,0 +1,37 @@
+//
+// Created by Kim Yang on 2020/8/26.
+// Copyright (c) Kim Yang All rights reserved.
+//
+#include <stdio.h>
+//树——双亲表示法（顺序存储）
+/**定义模块**/
+#define MAX_TREE_SIZE 100
+
+typedef struct {
+    int data; //数据元素
+    int parent;//双亲位置域
+}PTNode;
+typedef struct {
+    PTNode nodes[MAX_TREE_SIZE];//双亲表示
+    int n;//结点数
+}PTree;
+/**定义模块**/
+
+
+/**实现模块**/
+//坐等填坑
+
+/**实现模块**/
+
+
+/**测试模块**/
+
+void testModule() {
+
+}
+
+/**测试模块**/
+int main() {
+
+    return 0;
+}
